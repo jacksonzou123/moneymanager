e4c1e87caebf7b456bca256eb284c35b098f485a
Jun Tao Lei <44855944+juntaolei@users.noreply.github.com>
2020-01-07 22:32:25 -0500

    u

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -11,15 +11,16 @@ BP = Blueprint('auth', __name__, url_prefix='/auth')
 def register():
     """Register user in this app"""
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username, password = request.form['username'], request.form['password']
+        print(username)
+        print(password)
         error = None
-        if g.db.execute(f'SELECT id FROM user WHERE username = "{username}"'
+        if g.db.execute(f'SELECT id FROM users WHERE username = "{username}"'
                         ).fetchone() is not None:
             error = f'Username {username} is already registered.'
         if error is None:
             g.db.execute(
-                f'INSERT INTO user(username, password) VALUES ("{username}", "{generate_password_hash(password)}")'
+                f'INSERT INTO users VALUES (NULL, "{username}", "{generate_password_hash(password)}")'
             )
             g.db.commit()
             return redirect('/auth/signin')
@@ -34,7 +35,7 @@ def login():
         username, password = request.form['username'], request.form['password']
         error = None
         user = g.db.execute(
-            f'SELECT * FROM user WHERE username = "{username}"').fetchone()
+            f'SELECT * FROM users WHERE username = "{username}"').fetchone()
         if user is None:
             error = f'Username {username} is incorrect.'
         elif not check_password_hash(user['password'], password):
--- flaskr/static/assets/auth.js
+++ flaskr/static/assets/auth.js
@@ -1,8 +1,8 @@
 const signin = `
-<form class='form-signin text-center'>
+<form class='form-signin text-center' action='/auth/signin' method='POST'>
   <h1 class='h3 mb-3 font-weight-normal'>Sign In</h1>
-  <input type='text' class='form-control' placeholder='Username' name='username' required autofocus>
-  <input type='password' class='form-control' placeholder='Password' name='password' required>
+  <input type='text' class='form-control' placeholder='Username' name='username' oninput='matchPassword()' required autofocus>
+  <input type='password' class='form-control' placeholder='Password' name='password' oninput='matchPassword()' required>
   <hr>
   <button class='btn btn-md btn-primary btn-block' type='submit'>Sign in</button>
   <button class='btn btn-md btn-danger btn-block' type='button' onClick='switchAuth("register")'>
@@ -12,11 +12,11 @@ const signin = `
 `;
 
 const register = `
-<form class='form-signin text-center'>
+<form class='form-signin text-center' action='/auth/register' method='POST'>
   <h1 class='h3 mb-3 font-weight-normal'>Register</h1>
-  <input type='text' class='form-control' placeholder='Username' name='username' required autofocus>
-  <input type='password' class='form-control' placeholder='Password' name='password' required>
-  <input type='password' class='form-control' placeholder='Confirm Password' name='confirmPassword' required>
+  <input type='text' class='form-control' placeholder='Username' name='username' oninput='matchPassword()' required autofocus>
+  <input type='password' class='form-control' placeholder='Password' name='password' oninput='matchPassword()' required>
+  <input type='password' class='form-control' placeholder='Confirm Password' oninput='matchPassword()' name='confirmPassword' required>
   <hr>
   <button class='btn btn-md btn-primary btn-block' type='submit'>
     Register
@@ -42,9 +42,26 @@ const switchAuth = target => {
   }
 };
 
+const matchPassword = _ => {
+  const but = document.querySelector('button');
+  const password = document.querySelector('input[name=password]').value;
+  const confirmPassword = document.querySelector('input[name=confirmPassword').value;
+  console.log(but.attributes)
+  console.log(confirmPassword)
+  if ((password !== confirmPassword) || (!password && !confirmPassword)) {
+    but.className = but.className + ' disabled';
+    but.attributes = but.setAttribute('disabled', '');
+  }
+  else {
+    but.className = but.className.replace('disabled', '');
+    but.attributes = but.removeAttribute('disabled');
+  }
+}
+
 window.onload = _ => {
   if (window.location.pathname.includes('signin'))
     switchAuth('signin');
   else
     switchAuth('register');
 };
+
