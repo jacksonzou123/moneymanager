e033f754ab4b68930d452c632ae4948f522700b9
Jeff Lin <jeff.andrew.lin@gmail.com>
2020-01-13 14:14:44 -0500

    yes stuff works but only in firefox

--- flaskr/static/assets/src/components.mjs
+++ flaskr/static/assets/src/components.mjs
@@ -21,25 +21,34 @@ export const transactionForm = props => {
 export const home = props => {
   return (
     `
-    <div class="container-fluid">
-      <div class="row mb-3">
-        <h2 class="hello-title">Hello, ${props.user.username}!</h2>
-      </div>
-      <div class="row mb-3">
-        <div class="col overflow-auto text-center">
-          <h4>Add Transaction</h4>
-          <a class='addButton' id='addTransaction' style='color: white'>+</a>
+    <div style="padding: 30px;">
+        <div class="container-fluid body-container">
+          <div class="row">
+            <div class="col-md">
+              <h2 class="hello-title">Hello, ${props.user.username}!</h2>
+            </div>
+            <div class="col-md">
+              <input class="form-control searchbar" type="text" placeholder="Search" aria-label="Search">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col col-md-custom" style="text-align: center">
+              <h4>Add Transaction</h4>
+              <a class='addButton' id='addTransaction' style='color: white'>+</a>
+            </div>
+          </div>
+          <div class="container rounded border">
+            <div class="row rounded">
+              <div class="col-md col-md-custom border">
+                <h4>Quick Stats</h4>
+              </div>
+              <div class="col-md col-md-custom border">
+                <h4>Tags</h4>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="row rounded">
-        <div class="col-md col-md-custom border">
-          <h4>Quick Stats</h4>
-        </div>
-        <div class="col-md col-md-custom border">
-          <h4>Tags</h4>
-        </div>
-      </div>
-    </div>
     `
   );
 };
@@ -112,10 +121,7 @@ export const app = props => {
           <span class='navbar-toggler-icon'></span>
         </button>
         <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-          <ul class='navbar-nav'>
-            <li class='nav-item'>
-              <input class="form-control" type="text" placeholder="Search">
-            </li>
+          <ul class='navbar-nav'> 
             <li class='nav-item'>
               <a type='button' id='toTransactions' class='navbar-link btn btn-link text-muted'>Transactions</a>
             </li>
