fc7cfa4ef9f7508b459e847398660da3295f7550
Jeff Lin <jeff.andrew.lin@gmail.com>
2020-01-14 19:37:32 -0500

    Juntao please fix

--- flaskr/static/assets/src/components.mjs
+++ flaskr/static/assets/src/components.mjs
@@ -1,32 +1,18 @@
-import { calendar } from './calendar.mjs';
-
 export const transactionForm = props => {
   return (
     `
       <div class='row my-3'>
         <h4 class='mx-auto'>Add New Transaction</h4>
       </div>
-      <div class='row mb-3'>
-        <div class='col-sm'>
-          <form class='d-flex flex-column form-group ml-auto' style='max-widtd: 330px;'>
-            <input type='text' class='form-control mb-3' name='name' placeholder='Transaction Name'>
-            <input type='date' class='form-control mb-3' name='date'>
-            <input type='number' class='form-control mb-3' name='amount' placeholder='Amount'>
-            <input type='text' class='form-control mb-3' name='note' placeholder='Note'>
-            <div class="d-flex flex-row mb-3">
-              <div class="flex-fill">
-                <select class="form-control" name="tag">
-                  <option value="NULL">None</option>
-                  ${props.tags.map(t => `<option>${t.tag_type}</option>`)}
-                </select>
-              </div>
-              <div class="justify-content-center">
-                <button type='button' class='btn btn-primary rounded ml-2'>New Tag</button>
-              </div>
-            </div>
-            <button type='button' name="button" id='submitTransaction' class='btn btn-md btn-success'>Submit New Transaction</button>
-          </form>
-        </div>
+      <div class='row mx-auto mb-3'>
+        <form class='d-flex justify-content-center flex-column form-group text-center m-auto' style='max-width: 330px;'>
+          <input type='text' class='form-control mb-3' name='name' placeholder='Transaction Name'>
+          <input type='date' class='form-control mb-3' name='date'>
+          <input type='number' class='form-control mb-3' name='amount' placeholder='Amount'>
+          <input type='text' class='form-control mb-3' name='note' placeholder='Note'>
+          <input type='text' class='form-control mb-3' name='tag' placeholder='Tag'>
+          <button type='button' id='submitTransaction' class='btn btn-md btn-primary'>Submit New Transaction</button>
+        </form>
       </div>
     `
   );
@@ -39,54 +25,24 @@ export const home = props => {
       <div class="row mb-3 mx-5">
         <h2 class="hello-title">Hello, ${props.user.username}!</h2>
       </div>
-      <div class="row mb-3 d-flex justify-content-center mx-3" style="padding-bottom: 30px;">
+      <div class="row mb-3 d-flex justify-content-center mx-3">
         <div class="text-center d-flex justify-content-center flex-column">
           <h4>Add Transaction</h4>
-          <button type="button" class='d-flex justify-content-center btn addButton text-white rounded mx-auto' id='addTransaction'>+</a>
+          <button type="button" class='font-weight-bold d-flex justify-content-center btn btn-lg addButton text-white rounded mx-auto' id='addTransaction'>+</a>
         </div>
       </div>
       <div class="card-group mx-5">
         <div class="card">
           <div class='card-body'>
             <h4>Quick Stats</h4>
-            <table>
-              <tr>
-                <td>Today:</td>
-                <td class='amount'>$72.05</td>
-              </tr>
-              <tr>
-                <td>Last 7 Days:</td>
-                <td class='amount'>something</td>
-              </tr>
-              <tr>
-                <td>This Month:</td>
-                <td class='amount'>something</td>
-              </tr>
-              <tr>
-                <td>This Year:</td>
-                <td class='amount'>something</td>
-              </tr>
-            </table>
           </div>
         </div>
         <div class="card">
           <div class='card-body'>
             <h4>Tags</h4>
-            <table>
-                ${props.tags.map(t => `
-                  <tr>
-                  <td>${t.tag_type}</td>
-                  <td class='amount'>something</td>
-                  </tr>
-                `).join('')}
-            </table>
           </div>
         </div>
-        <div>
-        ${calendar(props)}
-        </div>
       </div>
-    </div>
     `
   );
 };
@@ -102,8 +58,6 @@ export const transaction = props => {
         <button type='button' id='addTransaction' class='btn btn-sm btn-success rounded'>
           Add Transaction
         </button>
-        ${JSON.stringify(props)}
-        ${props.transaction.map(t => `<p>${JSON.stringify(t)}</p>`)}
       </div>
     </div>
 
@@ -111,12 +65,11 @@ export const transaction = props => {
     <div class="container-fluid p-0 m-0">
       <div class="card-group mx-5">
         <div class="card">
-          <div class='card-body'>
-          </div>
+        ${JSON.stringify(props)}
+        ${props.transaction.map(t => `<p>${JSON.stringify(t)}</p>`)}
         </div>
       </div>
     </div>
-
     `
   );
 };
@@ -137,7 +90,7 @@ export const todos = props => {
     <div>
     <button type='button' id='addTodo' class='btn btn-sm btn-primary rounded ml-auto'>
       Add Todo
-    </button>
+    </button >
     ${props.todos.map(t => JSON.stringify(t))}
     </div>
     `
@@ -173,39 +126,36 @@ export const app = props => {
   }
   return (
     `
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-      <button type='button' id='returnHome' class='navbar-brand btn btn-link'>Spendie</button>
-      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav'>
-        <span class='navbar-toggler-icon'></span>
-      </button>
-      <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-        <ul class='navbar-nav'>
-          <li class='nav-item'>
-            <input class="form-control" type="text" placeholder="Search">
-          </li>
-          <li class='nav-item'>
-            <a type='button' id='toTransactions' class='navbar-link btn btn-link text-muted'>Transactions</a>
-          </li>
-          <li class='nav-item'>
-            <a type='button' id='toRequests' class='navbar-link btn btn-link text-muted'>Requests</a>
-          </li>
-          <li class='nav-item'>
-            <a type='button' id='toTodos' class='navbar-link btn btn-link text-muted'>Todos</a>
-          </li>
-          <li class='nav-item'>
-            <a type='button' id='toSettings' class='navbar-link btn btn-link text-muted'>Settings</a>
-          </li>
-          <li class='nav-item'>
-            <a type='button' class='navbar-link btn btn-link text-muted' href='/logout'>Log Out</a>
-          </li>
-        </ul>
+      <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+        <button type='button' id='returnHome' class='navbar-brand btn btn-link'>Spendie</button>
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
+          <ul class='navbar-nav'>
+            <li class='nav-item'>
+              <a type='button' id='toTransactions' class='navbar-link btn btn-link text-muted'>Transactions</a>
+            </li>
+            <li class='nav-item'>
+              <a type='button' id='toRequests' class='navbar-link btn btn-link text-muted'>Requests</a>
+            </li>
+            <li class='nav-item'>
+              <a type='button' id='toTodos' class='navbar-link btn btn-link text-muted'>Todos</a>
+            </li>
+            <li class='nav-item'>
+              <a type='button' id='toSettings' class='navbar-link btn btn-link text-muted'>Settings</a>
+            </li>
+            <li class='nav-item'>
+              <a type='button' class='navbar-link btn btn-link text-muted' href='/logout'>Log Out</a>
+            </li>
+          </ul>
+        </div>
+      </nav >
+      <div class='p-5'>
+        <div class='d-flex justify-content-center flex-column container-fluid px-3'>
+          ${view()}
+        </div>
       </div>
-    </nav >
-    <div class='p-5'>
-      <div class='d-flex justify-content-center flex-column container-fluid px-3'>
-        ${view()}
-      </div>
-    </div>
     `
   );
 };
